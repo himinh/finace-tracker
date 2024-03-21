9f5b3530df23c8a3ea714aa7c5058d94f8630757
minh <minhchiu.official@gmail.com>
2024-03-22 01:22:00 +0700

    Trend Component (Showing Percentage Differnce)

--- components/trend.vue
+++ components/trend.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 interface IProp {
   title: string;
   amount: number;
@@ -7,7 +9,24 @@ interface IProp {
   loading: boolean;
 }
 
-defineProps<IProp>();
+const props = defineProps<IProp>();
+
+const trendingUp = computed(() => props.amount >= props.lastAmount);
+const icon = computed(() =>
+  trendingUp.value
+    ? 'i-heroicons-arrow-trending-up'
+    : 'i-heroicons-arrow-trending-down'
+);
+const percentageTrend = computed(() => {
+  if (props.amount === 0 || props.lastAmount === 0) return '♾️%';
+
+  const bigger = Math.max(props.amount, props.lastAmount);
+  const lower = Math.min(props.amount, props.lastAmount);
+
+  const ratio = ((bigger - lower) / lower) * 100;
+
+  return `${Math.ceil(ratio)}%`;
+});
 </script>
 
 <template>
@@ -25,13 +44,11 @@ defineProps<IProp>();
     <div>
       <USkeleton v-if="loading" class="h-6 w-full" />
       <div v-else class="flex space-x-1 items-center text-sm">
-        <UIcon
-          name="i-heroicons-arrow-trending-up"
-          class="w-6 h-6"
-          :class="[color]"
-        />
+        <UIcon :name="icon" class="w-6 h-6" :class="[color]" />
 
-        <div class="text-gray-500 dark:text-gray-400">30% vs last period</div>
+        <div class="text-gray-500 dark:text-gray-400">
+          {{ percentageTrend }} vs last period
+        </div>
       </div>
     </div>
   </div>
