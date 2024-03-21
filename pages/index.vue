129bba3be8b25ff92af7d3c8ee376ed898ae84a8
minh <minhchiu.official@gmail.com>
2024-03-22 00:37:15 +0700

    feat: summary section and period selection

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { transactionViewOptions } from '~/constants';
+
+const selectedView = ref(transactionViewOptions[1]);
+</script>
 
 <template>
-  <div>Index Page!</div>
+  <section class="flex items-center justify-between mb-10">
+    <h1 class="text-4xl font-extrabold">Summary</h1>
+
+    <USelectMenu v-model="selectedView" :options="transactionViewOptions" />
+  </section>
 </template>
 
 <style lang="css" scoped></style>
